2a271c7445616e3251ca1ab94914c24c924f79b3
John Butler <jbutler@tripadvisor.com>
2020-01-06 14:55:45 -0500

    repeating/stopping bugfixes

--- src/SoundPlayer.js
+++ src/SoundPlayer.js
@@ -4,21 +4,19 @@ class SoundPlayer {
   context;
   gainNode;
   oscillator;
-  started;
-  isRepeating;
   
   constructor() {
     this.context = new AudioContext();
     this.gainNode = this.context.createGain();
     this.gainNode.gain.value = 0.15;
     this.gainNode.connect(this.context.destination);
-    this.isRepeating = false;
   }
 
   _newOscillator() {
     const o = this.context.createOscillator();
     o.type = 'square';
     o.connect(this.gainNode);
+    o.isRepeating = false;
     return o;
   }
 
@@ -26,17 +24,19 @@ class SoundPlayer {
     this.oscillator = this._newOscillator();
     this.oscillator.frequency.value = freq;
     this.oscillator.start();
-    this.started = true;
+    this.oscillator.started = true;
     setTimeout(() => this.stop(), ms);
   }
   
   stop() {
     try {
-      this.oscillator && this.started && this.oscillator.stop(0);
+      if (this.oscillator && this.oscillator.started) {
+        this.oscillator.stop(0);
+        this.oscillator.stopped = true;
+      }
     } catch (e) {
       console.error(e);
     }
-    this.isRepeating = false;
   }
 
   /**
@@ -55,13 +55,14 @@ class SoundPlayer {
       }
       const runtime = freqsAndLengths.map(([freq, ms]) => ms).reduce((a, b) => a + b);
       this.oscillator.start();
-      this.started = true;
+      this.oscillator.started = true;
       this.oscillator.stop(startTime + runtime / 1000);
 
       const repeat = document.querySelector('input[type=checkbox]').checked;
       if (repeat) {
-        setTimeout(() => this.isRepeating && this.playMulti(freqsAndLengths), runtime);
-        this.isRepeating = true;
+        const o = this.oscillator;
+        o.isRepeating = true;
+        setTimeout(() => o.isRepeating && !o.stopped && this.playMulti(freqsAndLengths), runtime);
       }
     }
   }
